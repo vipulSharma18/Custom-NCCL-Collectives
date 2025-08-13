e9cc5c882a831c893e668e8e44db8231139cc3ab
Vipul Sharma <vipuls181999@gmail.com>
2025-08-13 16:52:33 -0400

    cmdOptions conflicts with mpi

--- src/common.h
+++ src/common.h
@@ -142,16 +142,4 @@ extern int is_main_proc;
 extern thread_local int is_main_thread;
 #define PRINT if (is_main_thread) printf
 
-char* getCmdOption(char** begin, char** end, const std::string& option){
-    char** itr = std::find(begin, end, option);
-    if(itr != end && ++itr != end){
-        return *itr;
-    }
-    return 0;
-}
-
-bool cmdOptionExists(char** begin, char** end, const std::string& option){
-    return std::find(begin, end, option) != end;
-}
-
 #endif
